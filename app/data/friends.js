c6c3bf8d7bf35d585780aa9bc1c71c3b536a21c2
beepritchard <sirin15@mac.com>
2020-03-20 16:42:55 -0500

    Add files

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		name: "Berny",
+		photo: "https://freedesignfile.com/upload/2016/03/Cute-cartoon-crocodile-vector.jpg",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Bob",
+		photo: "https://i.pinimg.com/originals/42/73/f6/4273f60136a294f378f0205ba84de7fe.jpg",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Peet",
+		photo: "https://freedesignfile.com/upload/2012/09/Children-and-animals-cartoon-vector-illustration-2.jpg",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Rob",
+		photo: "https://www.freepik.com/free-vector/yellow-bunny-free-vector_565660.htm",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Sheq",
+		photo: "https://www.freepik.com/free-photo/adorable-beagle-puppy-solo-portrait_3532462.htm",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	}
+];
+
+module.exports = friends;
